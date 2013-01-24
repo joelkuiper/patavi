91ed29c191251ce18644415ce3d7979d305474aa
Joël Kuiper <me@joelkuiper.eu>
2013-01-24 17:58:17 +0100

    Add bootrap and related directives
    Switch to hosted CDN for JS includes

--- client-side/app/js/app.js
+++ client-side/app/js/app.js
@@ -2,9 +2,10 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('cliniccio', ['cliniccio.filters', 'cliniccio.services', 'cliniccio.directives']).
+angular.module('cliniccio', ['cliniccio.filters', 'cliniccio.services', 'cliniccio.directives', '$strap.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    //$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
+    //$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
+    //$routeProvider.otherwise({redirectTo: '/view1'});
   }]);
+
