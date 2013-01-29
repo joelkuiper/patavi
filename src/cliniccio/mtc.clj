4fb7ae61b12121f792dddb2497c20e372b518a66
Joel Kuiper <me@joelkuiper.eu>
2013-01-29 14:28:21 +0100

    Convert network to Clojure maps

--- src/cliniccio/mtc.clj
+++ src/cliniccio/mtc.clj
@@ -1,11 +1,11 @@
 (ns cliniccio.mtc
-  (:require [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log])
   (:import (org.rosuda.REngine)
            (org.rosuda.REngine.Rserve RConnection)))
            
 (defn- load-mtc! [conn] 
-  (.voidEval conn "suppressWarnings(require('gemtc',quietly=TRUE))")
-)
+  (.voidEval conn "suppressWarnings(require('gemtc',quietly=TRUE))"))
 
 (defn R-connection [] 
   (let [conn (try 
@@ -24,11 +24,28 @@
       (.voidEval R (str "network <- read.mtc.network('" (file :filename) "')")))
     R))
 
+(defn rlist [data field convert-fn]
+  (let [members (.at data field)]
+    (if-not (nil? members)
+      (seq (convert-fn members))
+      nil))) 
+
+(defn transpose-map [maps]
+  (let [x (into {} (filter second maps))] ;; Second is false when nil, so this removes nil values
+    (map (fn [m] (zipmap (keys x) m)) (apply map vector (vals x)))))
+
 (defn convert-network [file & args] 
   (let [R (load-network (R-connection) file)
         description (.eval R "network$description")
-        data (.asList (.eval R "network$data"))]
+        data (.asList (.eval R "network$data"))
+        treatments (.asList (.eval R "network$treatments"))]
     {:description (if-not (.isNull description) (.asString description) "")
-     :data {:study (seq (.asStrings (.asFactor (.at data "study" ))))
-            :sampleSize (seq (.asIntegers (.at data "sampleSize" )))}
+     :data (transpose-map {:study (rlist data "study" (comp #(.asStrings %) #(.asFactor %)))
+                           :treatment (rlist data "treatment" (comp #(.asStrings %) #(.asFactor %)))
+                           :sampleSize (rlist data "sampleSize" #(.asIntegers %))
+                           :responders (rlist data "responders" #(.asIntegers %))
+                           :mean (rlist data "mean" #(.asDoubles %))
+                           :std.dev (rlist data "std.dev" #(.asDoubles %))})
+     :treatments (transpose-map {:id (rlist treatments "id" (comp #(.asStrings %) #(.asFactor %)))
+                                 :description (rlist treatments "description" (comp #(.asStrings %) #(.asFactor %)))})
      :success true}))
