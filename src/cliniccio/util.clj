e1a6bedcdac03d34774a606d52c30c35a1d9b477
Joel Kuiper <me@joelkuiper.eu>
2013-01-30 15:29:05 +0100

    Add the inverse of cols-to-rows
    - Rename function

--- src/cliniccio/util.clj
+++ src/cliniccio/util.clj
@@ -1,6 +1,11 @@
 
 (ns cliniccio.util) 
 
-(defn transpose-map [maps]
-  (let [x (into {} (filter second maps))] ;; Second is false when nil, so this removes nil values
+(defn map-cols-to-rows [maps]
+  (let [x (into {} (filter second maps))] ;; nil is falsey, so this removes nil values
     (map (fn [m] (zipmap (keys x) m)) (apply map vector (vals x)))))
+
+(defn map-rows-to-cols [maps]
+  (reduce (fn [m1 m2]
+    (reduce (fn [m [k v]] 
+      (update-in m [k] (fnil conj []) v)) m1, m2)) {} maps))
