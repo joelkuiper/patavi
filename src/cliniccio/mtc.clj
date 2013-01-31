87a09ae6fd2b01204524e44433ef0423cd5ae586
Joël Kuiper <me@joelkuiper.eu>
2013-01-31 19:39:14 +0100

    Parse results for consistency model

--- src/cliniccio/mtc.clj
+++ src/cliniccio/mtc.clj
@@ -1,11 +1,13 @@
 (ns cliniccio.mtc
-  (:use     [cliniccio.util]) 
+  (:use [cliniccio.util]
+        [clojure.string :only [upper-case join split]]) 
   (:require [clojure.java.io :as io]
             [clojure.tools.logging :as log]
             [cliniccio.R.util :as R])
   (:import (org.rosuda.REngine)
+           (org.rosuda.REngine REXP
+                               RList)
            (org.rosuda.REngine.Rserve RConnection)))
-
 
 (defn load-mtc! [R] 
   (.voidEval R "suppressWarnings(require('gemtc',quietly=TRUE))"))
@@ -34,10 +36,25 @@
      :treatments (map-cols-to-rows {:id (R/in-list treatments "id" (comp #(.asStrings %) #(.asFactor %)))
                                  :description (R/in-list treatments "description" (comp #(.asStrings %) #(.asFactor %)))})}))
 
+(defn parse-results-list [lst] 
+  (let [names (.keys lst)
+        conv {"matrix" #(R/parse-matrix %)}]
+    (map (fn [k] (let [itm (.asList (.at lst k))
+                       data (.at itm "data")
+                       data-type (.asString (.at itm "type"))]
+                    (log/debug "got results for" k " with " data " as " data-type)
+                    ((get conv data-type) data)
+                   )) names)))
+
+(defn parse-results [^REXP results]
+  (let [data (.asList results)]
+    {:images (R/in-list data "images" #(.asStrings %))
+     :results (parse-results-list (.asList (.at data "results")))}))
  
 (defn consistency [R & args] 
-  (do 
-    (.voidEval R "model <- mtc.model(network)")
-    ;(R/plot R "model")
-    ;(R/plot R "network"))
-  {:network (read-network R)}))
+  (let [script-file "consistency.R"] 
+    (with-open [script (.createFile R script-file)] 
+      (io/copy (io/as-file (io/resource (str "R/" script-file))) script))
+    (.voidEval R (str "source('"script-file"')"))
+    (.removeFile R script-file)
+    {:results (parse-results (R/parse R "mtc.consistency(network)"))}))
