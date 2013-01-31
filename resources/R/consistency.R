9514d2b1c0e3a1f1349a0f4800a53cb0ea8c9079
Joël Kuiper <me@joelkuiper.eu>
2013-01-31 14:22:17 +0100

    Expand tabs to spaces
    I like to enforce my personal preference here.
    Remove unneeded line

--- resources/R/consistency.R
+++ resources/R/consistency.R
@@ -1,33 +1,31 @@
-mtc.consistency <- function(network, factor = 2.5 , n.chain = 4, 
-														n.adapt = 5000, n.iter = 20000, thin = 1)  {
-	model <- mtc.model(network, "Consistency",  factor, n.chain) 
-	run <- mtc.run(model, sampler="YADAS", n.adapt=n.adapt, n.iter=n.iter, thin=thin) 
-	
-	wrap.result <- function(result) { 
-		list(data=result, type= class(result))
-	}
-	quantiles <- summary(run)$quantiles 
-	psrf <- gelman.diag(run)$psrf 
-	rank.prob <- rank.probability(run) 
-	
-	wrap.plot <- function(plot.fn, name) {
-		name <- paste(name, ".png", sep="")
-		try(png(name))
-		plot.fn() 
-		dev.off();
-		paste(getwd(), "/", name, sep="")
-	}
+mtc.consistency <- function(network, factor = 2.5 , n.chain = 4, n.adapt = 5000, n.iter = 20000, thin = 1)  {
+  model <- mtc.model(network, "Consistency",  factor, n.chain) 
+  run <- mtc.run(model, sampler="YADAS", n.adapt=n.adapt, n.iter=n.iter, thin=thin) 
+  
+  wrap.result <- function(result) { 
+    list(data=result, type= class(result))
+  }
+  quantiles <- summary(run)$quantiles 
+  psrf <- gelman.diag(run)$psrf 
+  rank.prob <- rank.probability(run) 
+  
+  wrap.plot <- function(plot.fn, name) {
+    name <- paste(name, ".png", sep="")
+    try(png(name))
+    plot.fn() 
+    dev.off();
+    paste(getwd(), "/", name, sep="")
+  }
 
-	plots <- list("forest" = (function() forest(run)),
-								"model" = (function() plot(model)),
-								"network" = (function() plot(network)), 
-								"ranks" = (function() barplot(t(rank.prob), col=rainbow(20), beside=T, legend.text=paste("Rank", rep(1:dim(rank.prob)[[1]])))))
+  plots <- list("forest" = (function() forest(run)),
+                "model" = (function() plot(model)),
+                "network" = (function() plot(network)), 
+                "ranks" = (function() barplot(t(rank.prob), col=rainbow(20), beside=T, legend.text=paste("Rank", rep(1:dim(rank.prob)[[1]])))))
 
-	results <- list("quantiles" = quantiles, 
-									"psrf" = psrf, 
-									"ranks" = rank.prob)
-
-	i <- 1;
-	list(results=lapply(results, function(x) { wrap.result(x) }),
-			 images=mapply(function(name, plot.fn) { wrap.plot(plot.fn, name) }, names(plots), plots))
+  results <- list("quantiles" = quantiles, 
+                  "psrf" = psrf, 
+                  "ranks" = rank.prob)
+  
+  list(results=lapply(results, function(x) { wrap.result(x) }),
+       images=mapply(function(name, plot.fn) { wrap.plot(plot.fn, name) }, names(plots), plots))
 }
