41d65284d8b917a5dd60fab7e33a32525099f657
Joel Kuiper <me@joelkuiper.eu>
2013-02-04 17:06:59 +0100

    Save and retrieve data from MongoDB

--- src/cliniccio/mtc/store.clj
+++ src/cliniccio/mtc/store.clj
@@ -1,6 +1,10 @@
 (ns cliniccio.mtc.store
+  (:import [org.bson.types ObjectId]
+           (com.fasterxml.jackson.core JsonGenerator))
   (:use [monger.core :only [connect! set-db! get-db]]
         [monger.result :only [ok?]]
+        [monger.conversion :only [from-db-object]]
+        [cheshire.custom :only [JSONable]]
         [cliniccio.config]
         [cliniccio.util]
         [validateur.validation])
@@ -19,6 +23,15 @@
    :db "cliniccio" 
    :results-collection "results"})
 
+(extend org.joda.time.DateTime
+  JSONable
+  {:to-json (fn [^org.joda.time.DateTime date ^JsonGenerator jg]
+    (.writeStartObject jg)
+    (.writeFieldName jg "date")
+    (.writeString jg (.toString date))
+    (.writeEndObject jg))})
+
+
 (connect! mongo-options)
 (set-db! (get-db (mongo-options :db)))
 
@@ -36,13 +49,16 @@
                       (presence-of :network)
                       (presence-of :results)))
 
-(defn save-results [result]
+(defn get-result [id] 
+  (dissoc (collection/find-map-by-id "results" (ObjectId. id)) :_id))
+
+(defn save-result [result]
   (let [new-result (created-now 
                      (modified-now (with-oid result)))]
     (if (valid? result-validator new-result)
       (if (ok? 
             (collection/insert 
               (mongo-options :results-collection) (conv/to-db-object (stringify-keys* new-result))))
-        {:results (str base-url "/api/results/" (str (new-result :_id)))} 
+        {:results (str base-url "api/result/" (str (new-result :_id)))} 
         (throw (Exception. "Write Failed")))
       (throw (IllegalArgumentException.)))))  
