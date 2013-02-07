4ffc8e3c3ff86b7ab3bdfc6ec32ca3498c20a00d
Joël Kuiper <me@joelkuiper.eu>
2013-02-07 01:44:03 +0100

    Add a crude but effective doc script

---
+++ update-docs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp docs/uberdoc.html /tmp/index.html
+git stash
+git checkout gh-pages
+mv /tmp/index.html . 
+git commit -a -m "Update documentation" 
+git push origin gh-pages
+git checkout master
+git stash pop
