3ba4e25a9d22cdb69c6badf08cab7dee38a0b676
Joël Kuiper <me@joelkuiper.eu>
2013-03-11 10:49:23 +0100

    More uniform way of creating file paths for images

--- src/clinicico/R/store.clj
+++ src/clinicico/R/store.clj
@@ -1,5 +1,5 @@
-;; ## Store 
-;; A simple MongoDB storage for the produced results. 
+;; ## Store
+;; A simple MongoDB storage for the produced results.
 ;; Takes any map that includes a results field.
 
 (ns clinicico.R.store
@@ -30,15 +30,15 @@
 (connect! mongo-options)
 (set-db! (get-db (mongo-options :db)))
 
-(defn- with-oid 
+(defn- with-oid
   [results]
   (assoc results :_id (util/object-id)))
 
-(defn- created-now 
+(defn- created-now
   [results]
   (assoc results :created (time/now)))
 
-(defn- modified-now 
+(defn- modified-now
   [result]
   (assoc result :modified (time/now)))
 
@@ -46,58 +46,63 @@
                       (presence-of :_id)
                       (presence-of :results)))
 
-(defn- get-filename 
+(defn- get-filename
   [file]
   (let [ext (get-in file [:data :metadata "format"])
         name (:name file)]
     (str name "." ext)))
 
-(defn- save-file 
-  [file analysis id]
+(defn- save-file
+  [file path]
     (store-file (make-input-file (get-in file [:data :content]))
                 (metadata (get-in file [:data :metadata]))
                 (content-type (get-in file [:data :mime]))
-                (filename (str id "/" analysis "/" (get-filename file)))))
+                (filename path)))
 
-(defn save-files-for-result 
+(defn- get-path
+  [id directory file]
+ (str "result/" id "/file/" directory "/" file))
+
+(defn save-files-for-result
   [result id analysis]
   (let [filter-fn (fn [x] (not (nil? (get-in x [:data :mime]))))
-        files (filter filter-fn result)]
-    (doall (map (fn [f] (save-file f analysis id)) files))
+        files (filter filter-fn result)
+        path-fn (fn [file] (get-path id analysis (get-filename file)))]
+    (doall (map (fn [file] (save-file file (path-fn file))) files))
     (concat (remove filter-fn result)
-      (map (fn [file] 
-        {:name (:name file) 
+      (map (fn [file]
+        {:name (:name file)
          :description (:description file)
-         :url (str api-url "result/" id "/file/" analysis "/" (get-filename file))}) files))))
+         :url (str api-url (path-fn file))}) files))))
 
-(defn save-files 
+(defn save-files
   [results]
-  (assoc results :results 
-    (into {} (map (fn [[k v]] 
+  (assoc results :results
+    (into {} (map (fn [[k v]]
       {k (save-files-for-result v(:_id results) (name k))}) (results :results)))))
 
-(defn save-result 
+(defn save-result
   [results]
-  (let [new-result (created-now 
+  (let [new-result (created-now
                      (modified-now (save-files (with-oid results))))]
     (if (valid? result-validator new-result)
-      (if (ok? 
-            (collection/insert 
-              (mongo-options :results-collection) 
+      (if (ok?
+            (collection/insert
+              (mongo-options :results-collection)
               (conv/to-db-object (stringify-keys* new-result))))
         {:results (str api-url "result/" (str (new-result :_id)))
          :id (str (new-result :_id))
-         :completed (time/now)} 
+         :completed (time/now)}
         (throw (Exception. "Write Failed")))
-      (throw (IllegalArgumentException. "Could not save invalid result set")))))  
+      (throw (IllegalArgumentException. "Could not save invalid result set")))))
 
-(defn get-result 
-  [id] 
+(defn get-result
+  [id]
   (dissoc (collection/find-map-by-id "results" (ObjectId. id)) :_id))
 
-(defn get-file 
-  [id analysis filename] 
-  (let [f (-> (gfs/find-one {:filename (str id "/" analysis "/" filename)}))]
-    (if (nil? f) 
+(defn get-file
+  [id analysis filename]
+  (let [f (-> (gfs/find-one {:filename (get-path id analysis filename)}))]
+    (if (nil? f)
       (throw (clinicico.ResourceNotFound.))
       f)))
