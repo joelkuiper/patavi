4a6954dd4ca945f2838a325179516ad6bdaf4bf2
Joel Kuiper <me@joelkuiper.eu>
2013-05-07 23:21:37 +0200

    Add log4j capabilities

--- clinicico.server/project.clj
+++ clinicico.server/project.clj
@@ -5,6 +5,8 @@
                  [compojure "1.1.5"]
                  [ring-middleware-format "0.2.3"]
                  [ring/ring-json "0.1.2"]
+                 [org.clojure/tools.logging "0.2.4"]
+                 [log4j/log4j "1.2.17"]
                  [com.novemberain/langohr "1.0.0-beta13"]]
   :plugins [[lein-ring "0.8.3"]]
   :ring {:handler clinicico.server.handler/app
--- clinicico.worker/project.clj
+++ clinicico.worker/project.clj
@@ -5,5 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.2"]
+                 [org.clojure/tools.logging "0.2.4"]
+                 [log4j/log4j "1.2.17"]
                  [com.novemberain/langohr "1.0.0-beta13"]]
   :main clinicico.worker)
