d5608cdda6d19a034b0318d08a99fc8f33dc9383
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-05-07 10:58:59 +0200

    SMAA: take problem from input
     - Remove fixed example

--- resources/R/smaa.R
+++ resources/R/smaa.R
@@ -17,59 +17,6 @@ make.plot <- function(plot.fn) {
     img
 }
 
-# Basic example for UI prototyping
-init.example <- function() {
-	N <- 10000
-	n <- 3
-	m <- 2
-
-	# "Benefit": proximal DVT risk (p)
-	# "Benefit": distal DVT risk (d)
-	# "Risk": major bleeding risk (b)
-
-	# Heparin
-	m.p0 <- rbeta(n=N,20,116)
-	m.d0 <- rbeta(n=N,40,96)
-	m.b0 <- rbeta(n=N,1,135)
-
-	# Enoxaparin
-	m.p1 <- rbeta(n=N,8,121)
-	m.d1 <- rbeta(n=N,32,97)
-	m.b1 <- rbeta(n=N,5,124)
-
-	# Measurement scales (worst value first)
-	s.p <- c(0.25, 0.0)
-	s.d <- c(0.4, 0.15)
-	s.b <- c(0.1, 0.0)
-
-	# Partial value functions
-	partialValue <- function(worst, best) { 
-		if (best > worst) {
-			function(x) {
-				(x - worst) / (best - worst)
-			}
-		} else {
-			function(x) {
-				(worst - x) / (worst - best)
-			}
-		}
-	}
-	u.p <- partialValue(s.p[1], s.p[2])
-	u.d <- partialValue(s.d[1], s.d[2])
-	u.b <- partialValue(s.b[1], s.b[2])
-
-	meas <- array(c(
-		u.p(m.p0), u.p(m.p1),
-		u.d(m.d0), u.d(m.d1),
-		u.b(m.b0), u.b(m.b1)), dim=c(N,m,n))
-
-	alts <- c("Heparin", "Enoxaparin")
-	crit <- c("Prox DVT", "Dist DVT", "Bleed")
-	dimnames(meas) <- list(NULL, alts, crit)
-
-	list(N=N, n=n, m=m, meas=meas, alts=alts, crit=crit)
-}
-
 harSample <- function(constr, n , N) {
     transform <- simplex.createTransform(n)
     constr <- simplex.createConstraints(transform, constr)
@@ -77,9 +24,44 @@ harSample <- function(constr, n , N) {
     har(seedPoint, constr, N=N * (n-1)^3, thin=(n-1)^3, homogeneous=TRUE, transform=transform)$samples
 }
 
+partialValue <- function(worst, best) { 
+	if (best > worst) {
+		function(x) {
+			(x - worst) / (best - worst)
+		}
+	} else {
+		function(x) {
+			(worst - x) / (worst - best)
+		}
+	}
+}
+
 smaa <- function(params) { 
-	input <- init.example()
-	attach(input)
+	N <- 10000
+	n <- length(params$criteria)
+	m <- length(params$alternatives)
+	crit <- names(params$criteria)
+	alts <- names(params$alternatives)
+
+	pvf <- lapply(params$criteria, function(criterion) {
+		range <- criterion$pvf$range
+		if (criterion$pvf$type == 'linear-increasing') {
+			return(partialValue(range[1], range[2]))
+		} else if (criterion$pvf$type == 'linear-decreasing') {
+			return(partialValue(range[2], range[1]))
+		} else {
+			stop(paste("PVF type '", criterion$pvf$type, "' not supported.", sep=''))
+		}
+	})
+
+	meas <- array(dim=c(N,m,n), dimnames=list(NULL, alts, crit))
+	for (m in params$performanceTable) {
+		if (m$performance$type == 'dbeta') {
+			meas[, m$alternative, m$criterion] <- pvf[[m$criterion]](rbeta(N, m$performance$parameters$alpha, m$performance$parameters$beta))
+		} else {
+			stop(paste("Performance type '", m$performance$type, "' not supported.", sep=''))
+		}
+	}
 
 	# parse preference information
 	constr <- do.call(mergeConstraints, lapply(params$preferences,
@@ -99,17 +81,15 @@ smaa <- function(params) {
 		})
 	)
 
-	print(constr)
-
 	weights <- harSample(constr, n, N)
 
 	utils <- smaa.values(meas, weights)
 	ranks <- smaa.ranks(utils)
 
 	results <- list(
-		results = list("preferences"=params,
+		results = list("parameters"=params,
 					   "ranks_plot"=make.plot(function() plot(ranks))),
-		descriptions = list("Preferences", "Rank acceptabilities")
+		descriptions = list("Parameters", "Rank acceptabilities")
 	)
 	
     mapply(wrap.result,
