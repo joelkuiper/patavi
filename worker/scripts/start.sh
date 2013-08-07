a993cc6863def304556bf7d1aa9c22134e7c28b3
Joël Kuiper <me@joelkuiper.eu>
2013-08-07 11:50:20 +0200

    Redirect stderr to stdout

--- worker/scripts/start.sh
+++ worker/scripts/start.sh
@@ -5,7 +5,7 @@ then
 	exit 1
 else
 	workdir=`cd "tmp";pwd`
-	nohup R CMD Rserve --RS-workdir ${workdir} --RS-conf resources/Rserve.conf --vanilla > logs/rserve.log 2> logs/rserve.err < /dev/null &
+	nohup R CMD Rserve --RS-workdir ${workdir} --RS-conf resources/Rserve.conf --vanilla > logs/rserve.log 2>&1 &
 	echo "starting with generated bootstrap.R (PID:" `pidof Rserve`")"
 	exit 0
 fi
