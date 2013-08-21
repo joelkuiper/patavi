612cb0fda4210e506cb01622996d2dcc564780ae
Joël Kuiper <me@joelkuiper.eu>
2013-08-21 17:40:29 +0200

    Depend on own Rserve fork

--- worker/project.clj
+++ worker/project.clj
@@ -1,12 +1,11 @@
 (defproject clinicico.worker "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :repositories {"local" ~(str (.toURI (java.io.File. "third-party/repo")))}
+  :description ""
+  :repositories {"drugis" "http://drugis.org/mvn"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clinicico.common "0.1.0-SNAPSHOT"]
                  [cheshire "5.2.0"]
-                 [local/RserveEngine "1.7.0"]
-                 [local/REngine "1.7.0"]]
+                 [org.rosuda/REngine "1.7.1-SNAPSHOT"]]
   :main clinicico.worker.main)
