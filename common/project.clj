312d5009617682ffa27bea4aff5ce53b961a81a2
Joel Kuiper <me@joelkuiper.eu>
2013-08-27 20:25:30 +0200

    Bump version

--- common/project.clj
+++ common/project.clj
@@ -1,4 +1,4 @@
-(defproject patavi.common "0.2.0"
+(defproject patavi.common "0.2.1"
   :description "Library with common functions and dependencies for server and worker"
   :url "http://patavi.com"
   :license {:name "The MIT License"
