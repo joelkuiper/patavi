7729d35aaf5c6f22be5fdeaa9aa975be2b43fc47
Joel Kuiper <me@joelkuiper.eu>
2013-09-05 15:14:45 +0200

    Add print statement to slow demo

--- worker/resources-dev/pirate/slow.R
+++ worker/resources-dev/pirate/slow.R
@@ -1,6 +1,7 @@
 slow <- function(params) {
   N <- 100;
   x <- abs(rnorm(N, 0.001, 0.05))
+  print(paste("printing from slow", params[[1]]))
   for(i in as.single(1:N)) {
     self.oobSend(list(progress=i));
     Sys.sleep(x[[i]])
